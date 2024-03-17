9392257643487a96f0d6d7f0316da55775d5ad60
Nick22nd <yxdgg1119@gmail.com>
2024-03-18 07:21:12 +0800

    feat: add QR code generator

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,20 @@ import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 import { router } from './routes/routers'
 import { createPinia } from 'pinia'
+// import VueQrcode from '@chenfengyuan/vue-qrcode';
+import VueQrcode from './components/VueQrcode'
 // import './style.css'
 import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 const pinia = createPinia()
-createApp(App)
-  .use(router).use(ElementPlus).use(pinia).mount('#app')
+const app = createApp(App)
+
+// register component should be before mount
+// from https://stackoverflow.com/questions/66024797/vue-3-failed-to-resolve-component-with-global-components
+app.component(VueQrcode.name, VueQrcode);
+
+app.use(router).use(ElementPlus).use(pinia).mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
   })
-
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,40 @@
 <template>
-  <div>
+  <el-scrollbar height="90vh">
     <h2>{{ taskStore.playerTitle }}</h2>
     <el-input v-model="taskStore.playUrl" placeholder="Please input" @change="urlChange" />
-    <div ref="videoDom" class="border w-full h-[10rm]" />
-  </div>
+    <div ref="videoDom" class="border w-full"></div>
+    <div class="flex flex-col items-center">
+      <el-switch v-model="showQRCode" class="ml-2" width="30" active-text="show qr code" inactive-text="hide qr code" />
+      <VueQrcode v-show="showQRCode" :value="taskStore.playUrl || taskStore.urlPrefix" @ready="onReady"></VueQrcode>
+    </div>
+  </el-scrollbar>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { Ref, onMounted, ref, watch } from 'vue'
 import DPlayer from 'dplayer'
 import Hls from 'hls.js'
 import { useTaskStore } from '../stores';
+import { useStorage } from '@vueuse/core';
+const playHistory = useStorage('play-history', [], localStorage) as Ref<string[]>
 const taskStore = useTaskStore()
 const videoDom = ref(null)
+const showQRCode = ref(false)
 const dplayer = ref(null as DPlayer | null)
 watch(() => taskStore.playUrl, async (newUrl, oldUrl) => {
   console.log('url', newUrl, oldUrl)
+  let urlSet = new Set(playHistory.value)
+  urlSet.add(newUrl)
+  playHistory.value = Array.from(urlSet)
   if (dplayer.value) {
+    if (newUrl.startsWith('file://')) {
+      dplayer.value.switchVideo({
+        url: newUrl,
+        type: 'auto',
+        // @ts-ignore
+      }, undefined)
+
+    }
     dplayer.value.switchVideo({
       url: newUrl,
       type: 'customHls',
@@ -37,6 +55,9 @@ watch(() => taskStore.playUrl, async (newUrl, oldUrl) => {
 })
 onMounted(() => {
   console.log('mounted')
+  if (!taskStore.playUrl) {
+    taskStore.playUrl = playHistory.value.at(-1) || ''
+  }
   const dp = new DPlayer({
     container: videoDom.value,
     video: {
@@ -51,13 +72,15 @@ onMounted(() => {
         },
       },
     },
-    autoplay: true,
+    autoplay: false,
   })
   dplayer.value = dp
   // console.log('dp', dp.plugins.hls)
   // dp.play()
 })
-
+function onReady() {
+  console.log('QR onReady')
+}
 function urlChange() {
   console.log('urlChange', taskStore.playUrl)
   if (dplayer.value)
